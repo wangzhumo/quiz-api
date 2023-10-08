91eabf87beec578ae4527cf30af1ce1f74875437
wangzhumo <wangzhumoo@gmail.com>
2023-10-09 03:16:14 +0800

    pipe instance / transform class pipe -2

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,24 @@ import { AppModule } from './app.module'
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
 import { AllExceptionFilter } from './filters/all-exception.filter'
 import { Logger, ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
     const fastifyAdapter = new FastifyAdapter({
         logger: true,
     })
-    const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter)
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter, {
+        cors: true,
+    })
+    // Swagger
+    const config = new DocumentBuilder()
+        .setTitle('Testing API')
+        .setDescription('The Testing API')
+        .setVersion('1.0')
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('api', app, document)
+
     // get httpAdapter
     const httpAdapter = app.get(HttpAdapterHost)
 
